2d4decd2e4adde994e444ab46553c51a2d513ba1
Muzammil Sajid <pc.21695.muzammil@gmail.com>
2023-08-06 16:05:15 +0500

    update image picker functionality

--- lib/screens/finding.dart
+++ lib/screens/finding.dart
@@ -3,32 +3,93 @@ import 'package:e_commerce_app/screens/visual_search.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 
-class FindindScreen extends StatefulWidget {
-  FindindScreen({super.key});
+// class FindindScreen extends StatefulWidget {
+//   FindindScreen({super.key});
+
+//   @override
+//   State<FindindScreen> createState() => _FindindScreenState();
+// }
+
+// class _FindindScreenState extends State<FindindScreen> {
+//   @override
+
+//   //Functionality For Image Picker
+
+//   File? _image;
+
+//   Future getImage(ImageSource) async {
+//     final image = await ImagePicker().pickImage(source: ImageSource);
+
+//     if (image == null) return;
+//     final imageTemporary = File(image.path);
+
+//     //
+//     setState(() {
+//       this._image = imageTemporary;
+//     });
+//   }
+
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Color(0xff1E1F28),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             _image != null
+//                 ? Image.file(
+//                     _image!,
+//                     width: 120,
+//                     height: 100,
+//                   )
+//                 : Image.asset(
+//                     "assets/finding.png",
+//                     width: 230,
+//                     height: 230,
+//                   ),
+
+//             //
+//             SizedBox(
+//               height: 15,
+//             ),
+
+//             //Icon
+//             const Icon(
+//               Icons.search,
+//               size: 50,
+//               color: Colors.red,
+//             ),
+
+//             SizedBox(
+//               height: 10,
+//             ),
+//             //
+//             Text(
+//               "Finding Similar \n       results ...",
+//               style: Theme.of(context).textTheme.titleMedium,
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// FindingScreen
+
+// ...
+
+class FindingScreen extends StatefulWidget {
+  final File? image;
+
+  FindingScreen({required this.image});
 
   @override
-  State<FindindScreen> createState() => _FindindScreenState();
+  State<FindingScreen> createState() => _FindingScreenState();
 }
 
-class _FindindScreenState extends State<FindindScreen> {
+class _FindingScreenState extends State<FindingScreen> {
   @override
-
-  //Functionality For Image Picker
-
-  File? _image;
-
-  Future getImage(ImageSource) async {
-    final image = await ImagePicker().pickImage(source: ImageSource);
-
-    if (image == null) return;
-    final imageTemporary = File(image.path);
-
-    //
-    setState(() {
-      this._image = imageTemporary;
-    });
-  }
-
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xff1E1F28),
@@ -36,19 +97,19 @@ class _FindindScreenState extends State<FindindScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            _image != null
+            widget.image != null
                 ? Image.file(
-                    _image!,
-                    width: 120,
-                    height: 100,
+                    widget.image!,
+                    width: 250,
+                    height: 250,
                   )
                 : Image.asset(
                     "assets/finding.png",
-                    width: 230,
-                    height: 230,
+                    width: 250,
+                    height: 250,
                   ),
 
-            //
+            // ...
             SizedBox(
               height: 15,
             ),
